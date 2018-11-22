a713bfa378a524e5a4dfd7102664888eed8b32de
congo-quinine <congo.quinine@gmail.com>
2018-11-22 08:27:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Code your solution here:
+
+const driversWithRevenueOver = (driver, revenueCompare) => {
+
+  const driverNameArray = driver.filter(driver => driver.revenue > revenueCompare)
+
+  driverNameArray.map(x => x.name)
+
+  return driverNameArray;
+
+}
+
+
+const driverNamesWithRevenueOver = (driver, revenueCompare ) => {
+
+    let newDriver = [];
+
+      for (let i of driver){
+        if (i.revenue > revenueCompare){
+            newDriver.push(i.name)
+        }
+      }
+      return newDriver;
+}
+
+
+const exactMatch = (driver, compare) => {
+
+  const driverArray = driver.filter(driver => driver.name === compare.name || driver.revenue === compare.revenue);
+
+  return driverArray;
+
+}
+
+const exactMatchToList = (driver, compare) => {
+    let r = []
+    const driverArray = driver.filter(driver => driver.name === compare.name || driver.revenue === compare.revenue);
+
+      for (let i of driverArray){
+          r.push(i.name);
+      }
+      return r;
+}
